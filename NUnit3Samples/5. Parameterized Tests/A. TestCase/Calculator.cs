3c4e4746dc78ba3dd3c87d75ab2c5682f3f3d2b2
davembush <davembush@dmbcllc.com>
2015-12-11 20:13:31 -0500

    Started Chapter 5

---
+++ NUnit3Samples/5. Parameterized Tests/A. TestCase/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace NUnit3Samples._5._Parameterized_Tests.A._TestCase
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            checked
+            {
+                return a + b;
+            }
+        }
+    }
+}
---
+++ NUnit3Samples/5. Parameterized Tests/A. TestCase/GivenWeHaveACalculatorObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnit3Samples._5._Parameterized_Tests.A._TestCase
+{
+    [TestFixture]
+    public class GivenWeHaveACalculatorObject
+    {
+        Calculator SUT { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            SUT = new Calculator();
+        }
+
+        [Test]
+        [TestCase(3, 7, ExpectedResult = 10)]
+        [TestCase(-2, -3, ExpectedResult = -5)]
+        public int AddingTwoNumbersShouldReturnCorrectSum(int a, int b)
+        {
+            // When I add 3 and 7 together
+            var returnValue = SUT.Add(a, b);
+            return returnValue;
+        }
+
+        [Test]
+        public void AddingOneToMaxValueThrowsAnException()
+        {
+            var exception = Assert.Catch(() => SUT.Add(int.MaxValue, 1));
+            Assert.That(exception, Is.TypeOf<OverflowException>());
+        }
+    }
+}
